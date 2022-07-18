f09d94c3884d93c0b452d526bbde044d857cfb3a
Srinivasa Rao Bellamkonda <srenivas35@gmail.com>
2022-07-18 12:32:13 -0700

    This is First commit

---
+++ calc.py
@@ -0,0 +1,13 @@
+class Calculator:
+
+    def addition(self):
+        print(a + b)
+
+    def subtraction(self):
+        print(a - b)
+
+    def multiplication(self):
+        print(a * b)
+
+    def division(self):
+        print(a / b)
